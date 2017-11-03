f30f1012c67275963728256e48882739750e42dc
Logician724 <king2sairos2013@gmail.com>
2017-11-03 17:56:08 +0200

    feat: Finalize digital timer build

--- digital_timer_top.v
+++ digital_timer_top.v
@@ -16,5 +16,10 @@ output reg [3:0] seg_0, seg_1, seg_2;
 wire clk_2MHz;
 wire [7:0] count;
 wire [3:0] ones, tens, hundreds;
-
+clk_generator(clk_2MHz,clk_50MHz);
+counter_99(count,clk_2MHz,reset);
+binary_to_BCD(count,hundreds,tens,ones);
+segment_display HEX_0 (ones, seg_0);
+segment_display HEX_1 (tens, seg_1);
+segment_display HEX_2 (hundreds, seg_2);
 endmodule
