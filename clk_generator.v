cc0a5bc9183caa63528260a3f622d1f1b2d79275
Logician724 <king2sairos2013@gmail.com>
2017-11-03 09:48:04 +0200

    feat: Finalize clk_generator module note that the reset variable was erased since it was not asked for in the design.

--- clk_generator.v
+++ clk_generator.v
@@ -1,11 +1,29 @@
-module clk_generator (clk_50MHz,reset, clk_2MHz);
+module clk_generator (clk_50MHz, clk_2MHz);
 input clk_50MHz;
 reg [24:0] counter;
 output reg clk_2MHz;
+//this implementation assumes that the output clk starts as active low
 initial
    begin
       counter <= 0;
-      clk_50MHz <= 0;
+      clk_2MHz <= 0;
+   end
+always @(posedge clk_50MHz)
+   begin
+      if(counter == 12499999)
+         begin
+            clk_2MHz <= ~clk_2MHz;
+            counter <= 0;
+         end
+       else
+         begin
+            counter <= counter + 1;
+         end
+         
    end
 endmodule
-      
+/*
+*credits for this idea of clk generation goes to
+*@Peter Green
+*electronics stack exchange
+*/      
