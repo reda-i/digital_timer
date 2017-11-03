458a11473074eb470d8c9dcb9da94335658c7f76
Logician724 <king2sairos2013@gmail.com>
2017-11-03 17:25:47 +0200

    docs: Add segment_display general documentation

--- segment_display.v
+++ segment_display.v
@@ -1,5 +1,10 @@
+/*
+* This module decodes a BCD
+* number to its equivalent segment
+* display code according to the
+* implemented pin planning
+*/
 module segment_display (num, segment_code);
 input [3:0] num;
 output reg [7:0] segment_code;
-
 endmodule
