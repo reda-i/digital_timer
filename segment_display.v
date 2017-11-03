dcc6319edb20197efce1d9edfc39b7a24a1e1946
Logician724 <king2sairos2013@gmail.com>
2017-11-03 18:29:17 +0200

    Fix: fix case statement syntax

--- segment_display.v
+++ segment_display.v
@@ -10,16 +10,16 @@ output reg [7:0] segment_code;
 always @ (num)
    begin
       case(num)
-         0: 8'b11_00_00_00;
-         1: 8'b11_11_10_01;
-         2: 8'b10_10_01_00;
-         3: 8'b10_11_00_00;
-         4: 8'b10_01_10_01;
-         5: 8'b10_01_00_10;
-         6: 8'b10_00_00_10;
-         7: 8'b11_11_10_00;
-         8: 8'b10_00_00_00;
-         9: 8'b10_01_00_00;
+         0: segment_code =  8'b11_00_00_00;
+         1: segment_code = 8'b11_11_10_01;
+         2: segment_code = 8'b10_10_01_00;
+         3: segment_code = 8'b10_11_00_00;
+         4: segment_code = 8'b10_01_10_01;
+         5: segment_code = 8'b10_01_00_10;
+         6: segment_code = 8'b10_00_00_10;
+         7: segment_code = 8'b11_11_10_00;
+         8: segment_code = 8'b10_00_00_00;
+         9: segment_code = 8'b10_01_00_00;
       endcase
    end
 endmodule
