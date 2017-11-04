4d5f76e6ed44b3c774951baba613cecdcad0fb67
Logician724 <king2sairos2013@gmail.com>
2017-11-04 10:41:51 +0200

    refactor: Change clk_2MHz to clk_2Hz

--- clk_generator.v
+++ clk_generator.v
@@ -1,18 +1,18 @@
-module clk_generator (clk_2MHz, clk_50MHz);
+module clk_generator (clk_2Hz, clk_50MHz);
 input clk_50MHz;
 reg [24:0] counter;
-output reg clk_2MHz;
+output reg clk_2Hz;
 //this implementation assumes that the output clk starts as active low
 initial
    begin
       counter <= 0;
-      clk_2MHz <= 0;
+      clk_2Hz <= 0;
    end
 always @(posedge clk_50MHz)
    begin
       if(counter == 12499999)
          begin
-            clk_2MHz <= ~clk_2MHz;
+            clk_2Hz <= ~clk_2Hz;
             counter <= 0;
          end
        else
